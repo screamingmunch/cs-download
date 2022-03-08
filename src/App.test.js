037e3899e56aac7c059e292ac7d3ab269371ae4a
Kristine L <screamingmunch@gmail.com>
2022-03-08 14:38:03 -0800

    clean up more auto-generated code

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const el = screen.getByText('Booya');
+  const el = screen.getByText('Download Selected');
   expect(el).toBeInTheDocument();
 });
